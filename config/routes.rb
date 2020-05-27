e0398c3bf530b2cbeae082c08ebcabd62cad0326
marsha <marshaannon@gmail.com>
2020-05-26 20:01:38 -0400

    take out duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   
+  root to: "application#home"
   resources :pods
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "application#home"
+  
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'omniauth'}
+
+  resources :pods, only: [:show] do
+    resources :books, only: [:new, :show, :edit, :update, :destroy]
+  end
+  
+  
+  #pod_books GET /pod/pod_id/book/:id 
+  
 
   
 
